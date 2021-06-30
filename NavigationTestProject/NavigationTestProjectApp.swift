0bd75d6cabb52d6d051d73b4acd632e2830cb943
Sunny Singh <sunnysingh@posteo.de>
2021-06-30 22:29:34 +0200

    Initial Commit

---
+++ NavigationTestProject/NavigationTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTestProjectApp.swift
+//  NavigationTestProject
+//
+//  Created by Sunny Singh on 30.06.21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
